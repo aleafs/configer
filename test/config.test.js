93fccc15359fdccab35fbd76d3ebf79d47f9d60f
aleafs <zhangxc83@gmail.com>
2012-11-05 15:02:23 +0800

    Add autoreload support

--- test/config.test.js
+++ test/config.test.js
@@ -1,6 +1,7 @@
 /* vim: set expandtab tabstop=2 shiftwidth=2 foldmethod=marker: */
 
 var should  = require('should');
+var fs = require('fs');
 var config  = require(__dirname + '/../');
 
 describe('file config', function() {
@@ -26,7 +27,7 @@ describe('file config', function() {
   /* {{{ should_ini_config_works_fine() */
   it('should_ini_config_works_fine', function() {
     var cfg	= config.create(__dirname + '/ini/test_config_file.ini');
-    cfg.all().should.eql({
+    cfg._getAll().should.eql({
       'key1'    : '',
       'key2'    : 0.01,
       'key3'    : 10,
@@ -46,10 +47,41 @@ describe('file config', function() {
     cfg.find('sec1').should.eql({
       'default' : {
         'key1'  : 'aa',
-        'key3'  : 'bb',
+      'key3'  : 'bb',
       },
     });
     cfg.find('').should.eql({});
+  });
+  /* }}} */
+
+  /* {{{ should_file_autoload_works_fine() */
+  it('should_file_autoload_works_fine', function (done) {
+    var fname = __dirname + '/ini/config.json';
+    var cdata = {
+      'key1' : 'val1',
+    'key2' : 21.23,
+    'key3' : '21.23',
+    'key4' : [
+  {'a' : 'b', 'c' : [1, 2, 'x']}
+  ]
+    };
+    fs.writeFile(fname, JSON.stringify(cdata) + '\r\n', function (e) {
+      should.ok(!e);
+
+      var _me = config.create(fname, 2);
+      _me.get('key2').should.eql(21.23);
+      _me.get('key3').should.eql('21.23');
+
+      _me.on('reload', function () {
+        _me.get('key2').should.eql(23.21);
+        setTimeout(done, 10);
+      });
+
+      cdata.key2 = 23.21;
+      fs.writeFile(fname, JSON.stringify(cdata), function (e) {
+        should.ok(!e);
+      });
+    });
   });
   /* }}} */
 
